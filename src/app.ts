01ac21abc4fa541b2738bc0a212bbe1ec423b24b
Rajmal1 <aeis.gui@hotmail.com>
2022-09-14 15:58:11 -0300

    Built-in Generics

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-// Code goes here!
+const names: Array<string> = [];
+
+const promisse: Promise<string> = new Promise((resolve, reject) => {
+  try {
+    setTimeout(() => {
+      resolve("This is done");
+    }, 2000);
+  } catch (error) {
+    reject(error);
+  }
+});
+
+promisse.then((data) => {
+  data.split(" ");
+});
