f44ab3e90f5f201a14c448b33186ed2324c13d5e
Rajmal1 <aeis.gui@hotmail.com>
2022-09-15 10:45:20 -0300

    adding extends do generic types

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,23 @@
-const names: Array<string> = [];
+// const names: Array<string> = [];
 
-const promisse: Promise<string> = new Promise((resolve, reject) => {
-  try {
-    setTimeout(() => {
-      resolve("This is done");
-    }, 2000);
-  } catch (error) {
-    reject(error);
-  }
-});
+// const promisse: Promise<string> = new Promise((resolve, reject) => {
+//   try {
+//     setTimeout(() => {
+//       resolve("This is done");
+//     }, 2000);
+//   } catch (error) {
+//     reject(error);
+//   }
+// });
 
-promisse.then((data) => {
-  data.split(" ");
-});
+// promisse.then((data) => {
+//   data.split(" ");
+// });
+
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergeObj = merge({ name: "Max" }, { age: 30 });
+
+console.log(mergeObj.age);
